21653c3aa4efc42dd988cf004c824d38bbdaa230
karantyagi <tyagi.k@husky.neu.edu>
2018-06-12 11:02:53 -0400

    added services to fetch all courses, modules, lessons, topics for course navigation

--- src/app/course-navigator/course-navigator.component.ts
+++ src/app/course-navigator/course-navigator.component.ts
@@ -16,13 +16,61 @@ export class CourseNavigatorComponent implements OnInit {
   topics = [];
   widgets = [];
   selectedCourseId = 0;
-
+  selectedModuleId = 0;
+  selectedLessonId = 0;
+  selectedTopicId = 0;
+  selectedWidgetId = 0;
 
   selectCourse(courseId) {
     this.selectedCourseId = courseId;
-    this.service.findAllModulesForCourses(courseId)
+    this.selectedModuleId = 0;
+    this.selectedLessonId = 0;
+    this.selectedTopicId = 0;
+    this.selectedWidgetId = 0;
+    this.modules = [];
+    this.lessons = [];
+    this.topics = [];
+    this.widgets = [];
+    this.service.findAllModulesForCourse(courseId)
       .then(modules => this.modules = modules);
   }
+
+  selectModule(moduleId) {
+    this.selectedModuleId = moduleId;
+    this.selectedLessonId = 0;
+    this.selectedTopicId = 0;
+    this.selectedWidgetId = 0;
+    this.lessons = [];
+    this.topics = [];
+    this.widgets = [];
+    // alert('Course ID: ' + this.selectedCourseId + 'Module ID: ' + moduleId);
+    this.service.findAllLessonsForModule(this.selectedCourseId, moduleId)
+      .then(lessons => this.lessons = lessons);
+  }
+
+  selectLesson(lessonId) {
+    this.selectedLessonId = lessonId;
+    this.selectedTopicId = 0;
+    this.selectedWidgetId = 0;
+    this.topics = [];
+    this.widgets = [];
+    this.service.findAllTopicsForLesson(this.selectedCourseId, this.selectedModuleId, lessonId)
+      .then(topics => this.topics = topics);
+  }
+
+  selectTopic(topicId) {
+    this.selectedTopicId = topicId;
+    this.selectedWidgetId = 0;
+    this.widgets = [];
+    this.service.findAllWidgetsForTopic(this.selectedCourseId, this.selectedModuleId, this.selectedLessonId, topicId)
+      .then(widgets => this.widgets = widgets);
+  }
+
+  selectWidget(widgetId) {
+    this.selectedWidgetId = widgetId;
+    // alert('Widget : ' + widgetId);
+  }
+
   ngOnInit() {
     this.service.findAllCourses()
       .then(courses => {
--- src/app/services/course-navigator.service.client.ts
+++ src/app/services/course-navigator.service.client.ts
@@ -3,8 +3,20 @@ export class CourseNavigatorServiceClient {
     return fetch('http://localhost:8080/api/course')
       .then(response => response.json());
   }
-  findAllModulesForCourses(courseId) {
+  findAllModulesForCourse(courseId) {
     return fetch('http://localhost:8080/api/course/' + courseId + '/module')
+      .then(response => response.json());
+  }
+  findAllLessonsForModule(courseId, moduleId) {
+    return fetch('http://localhost:8080/api/course/' + courseId + '/module/' + moduleId + '/lesson')
+      .then(response => response.json());
+  }
+  findAllTopicsForLesson(courseId, moduleId, lessonId) {
+    return fetch('http://localhost:8080/api/course/' + courseId + '/module/' + moduleId + '/lesson/' + lessonId + '/topic')
+      .then(response => response.json());
+  }
+  findAllWidgetsForTopic(courseId, moduleId, lessonId, topicId) {
+    return fetch('http://localhost:8080/api/topic/' + topicId + '/widget')
       .then(response => response.json());
   }
 }
