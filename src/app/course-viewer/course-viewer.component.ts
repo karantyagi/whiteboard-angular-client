b55e5bf82c1c71e5574f2ad678206605f0f9d3c7
karantyagi <tyagi.k@husky.neu.edu>
2018-06-13 02:08:27 -0400

    added course viewer component

---
+++ src/app/course-viewer/course-viewer.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import {Course} from '../models/coruse.model.client';
+import {ActivatedRoute} from '@angular/router';
+import {CourseServiceClient} from '../services/course.service.client';
+
+@Component({
+  selector: 'app-course-viewer',
+  templateUrl: './course-viewer.component.html',
+  styleUrls: ['./course-viewer.component.css']
+})
+export class CourseViewerComponent implements OnInit {
+
+  constructor(private service: CourseServiceClient,
+              private route: ActivatedRoute) {
+    this.route.params.subscribe(params => this.loadCourse(params['courseId']));
+  }
+
+  course: Course = new Course();
+  loadCourse(courseId) {
+    console.log(courseId);
+    this.service.findCourseById(courseId)
+      .then(course => this.course = course);
+  }
+
+  ngOnInit() {
+  }
+
+}
