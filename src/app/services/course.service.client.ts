a1da7f2709c617d03aadadfc297ecc919c792ceb
karantyagi <tyagi.k@husky.neu.edu>
2018-06-21 09:43:46 -0400

    added no-cors to handle opaque response

--- src/app/services/course.service.client.ts
+++ src/app/services/course.service.client.ts
@@ -1,11 +1,17 @@
 export class CourseServiceClient {
   COURSE_URL = 'https://kt-course-manager-server.herokuapp.com/api/course';
   findAllCourses() {
-    return fetch(this.COURSE_URL)
+    return fetch(this.COURSE_URL,
+      {
+        mode: 'no-cors'
+      })
       .then(response => response.json());
   }
   findCourseById(courseId) {
-    return fetch(this.COURSE_URL + '/' + courseId)
+    return fetch(this.COURSE_URL + '/' + courseId,
+      {
+        mode: 'no-cors'
+      })
       .then(response => response.json());
   }
 }
