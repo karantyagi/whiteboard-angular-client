78c0f9ca971256c123e71803b4205e7dbd4b7090
karantyagi <tyagi.k@husky.neu.edu>
2018-06-21 09:55:00 -0400

    update build with CORS for java server

--- server.js
+++ server.js
@@ -7,10 +7,22 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/hello-world-app'));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin",
+    // "http://localhost:4200");
+    "https://kt-course-manager-server.herokuapp.com");
+  res.header("Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
 app.get('/*', function(req,res) {
 
   res.sendFile(path.join(__dirname,'/dist/hello-world-app/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 4200);
+app.listen(process.env.PORT);
--- src/app/services/course.service.client.ts
+++ src/app/services/course.service.client.ts
@@ -1,17 +1,11 @@
 export class CourseServiceClient {
   COURSE_URL = 'https://kt-course-manager-server.herokuapp.com/api/course';
   findAllCourses() {
-    return fetch(this.COURSE_URL,
-      {
-        mode: 'no-cors'
-      })
+    return fetch(this.COURSE_URL)
       .then(response => response.json());
   }
   findCourseById(courseId) {
-    return fetch(this.COURSE_URL + '/' + courseId,
-      {
-        mode: 'no-cors'
-      })
+    return fetch(this.COURSE_URL + '/' + courseId)
       .then(response => response.json());
   }
 }
